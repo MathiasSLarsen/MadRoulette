daa7a535d47b54e2be96403a33f4af17164eec0b
MathiasSLarsen <mathias.s.larsen99@gmail.com>
2019-11-02 16:39:12 +0100

    Add files via upload

---
+++ Madroulette/Madroulette/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Madroulette
+//
+//  Created by Mathias Larsen on 02/11/2019.
+//  Copyright © 2019 MadneZ. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Madroulette/Madroulette/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Madroulette
+//
+//  Created by Mathias Larsen on 02/11/2019.
+//  Copyright © 2019 MadneZ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
